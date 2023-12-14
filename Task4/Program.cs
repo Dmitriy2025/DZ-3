ea078bd11252715f5cce36ecfd0dd556ba954245
Dmitriy <dmitriykharitonov333@gmail.com>
2023-12-14 18:17:31 +0300

    Решил 4 задачу

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int number = new Random().Next(1, 10001);
+string NumberString = number.ToString();
+Console.WriteLine($"Сгенерированное число: {number}");
+
+int length = NumberString.Length;
+int [] result = new int[length];
+
+for (int j = length - 1; j >= 0; j--)
+{
+    result[j] = number % 10;
+    number /= 10;
+}
+
+Console.Write("Массив: ");
+Console.WriteLine(string.Join(", ", result));
